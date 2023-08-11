7eea902bb9435bbd1f935ffa4bb58a0f492e1554
Adnan <adbak.dev@gmail.com>
2023-08-12 00:07:25 +0200

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Who from "./components/Who";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
 import { styled } from "styled-components";
+import Test from "./components/Test";
 
 const Container = styled.div`
   height: 100vh;
@@ -26,6 +27,7 @@ function App() {
       <Who />
       <Works />
       <Contact />
+      <Test />
     </Container>
   );
 }
